5477953e6f49af9a5f30bc42cc0fe03c737b13ef
just-a-coder1000 <74711827+just-a-coder1000@users.noreply.github.com>
2021-06-07 13:45:24 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,18 @@
+class Ground {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.image = loadImage("gamingbackground1.jpg")
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("white");
+      image(this.image,0,0, this.width, this.height);
+    }
+  };
